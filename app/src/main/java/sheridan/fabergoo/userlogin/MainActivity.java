d455f39274bdacc9cc6f989a9a26292b869837bc
nfgrep <nfg2600@gmail.com>
2020-03-19 19:22:50 -0400

    Initial commit

---
+++ app/src/main/java/sheridan/fabergoo/userlogin/MainActivity.java
@@ -0,0 +1,14 @@
+package sheridan.fabergoo.userlogin;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
